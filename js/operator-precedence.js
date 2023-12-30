0e6e0fcae0b29ec18d37c43b5b097f5e96fb092f
christiandarvs <christiandarvin4@gmail.com>
2023-12-31 02:47:37 +0800

    add intro to js files

---
+++ js/operator-precedence.js
@@ -0,0 +1,10 @@
+let x, y;
+x = y = 25 - 10 - 5; // perform mathematical operation, assign the value to y, then assign the value of y to x (right to left)
+console.log(x, y);
+
+const CS_GRADE = 95;
+const MATH_GRADE = 94;
+const PHY_GRADE = 93;
+
+const FINAL_GRADE = (CS_GRADE + LIT_GRADE + PHY_GRADE) / 3;
+console.log(FINAL_GRADE);
---
+++ js/variables.js
@@ -0,0 +1,31 @@
+console.log("JavaScript");
+console.log(2024);
+
+let firstName = "Christian";
+console.log(firstName);
+
+let $language = "Python";
+console.log($language);
+
+const PI = 3.1415;
+console.log(PI);
+
+let nationality = "Filipino";
+console.log(nationality + "\n");
+
+// Camel Case Convention - first word is lowercase, then the 1st letter of the proceeding words are uppercase
+
+/** Not allowed:
+ * let 3year = 3;
+ * let else = 'hello'
+ * let function = 'meow'
+ */
+
+// Assignment:
+let country = "Philippines";
+let continent = "Asia";
+let population = 113.9;
+
+console.log(`Country: ${country}`);
+console.log(`Continent: ${continent}`);
+console.log(`Population: ${population}`);
