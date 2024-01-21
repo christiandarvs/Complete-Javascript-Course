6c1d8b9d2dd3e688fa22933bf49cfd03f23c6068
christiandarvs <christiandarvin4@gmail.com>
2024-01-22 03:50:46 +0800

    Add attempts counter and update score display

--- js/DOM_manipulation/guess_my_number/script.js
+++ js/DOM_manipulation/guess_my_number/script.js
@@ -10,11 +10,27 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 const guess = document.querySelector(".guess").value;
+const attempts = 5;
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
-  console.log(
-    `The user entered the number: ${document.querySelector(".guess").value}`
-  );
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  const randomNumber = Math.floor(Math.random() * 20) + 1;
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number";
+  } else if (guess === randomNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+  } else if (guess > randomNumber) {
+    document.querySelector(".message").textContent = "Too High...";
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else if (guess < randomNumber) {
+    document.querySelector(".message").textContent = "Too Low...";
+    score--;
+  }
 });
 
 /*
